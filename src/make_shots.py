e2924482719c0e35eb450791cbd3783b5e096c14
Mattia Rigiroli <45123055+MattiaRigi97@users.noreply.github.com>
2021-04-28 16:42:59 +0200

    Update make_shots.py

--- src/make_shots.py
+++ src/make_shots.py
@@ -20,30 +20,31 @@ def main():
         gtsummary = video_file['gtsummary'][...].astype(np.float32)
 
         seq_len = gtscore.size
-        n_frames = seq_len * 15 - 1
-        picks = np.arange(0, seq_len) * 15
+        n_frames = seq_len * 15 - 1 
+        picks = np.arange(0, seq_len) * 15 # array([    0,    15,    30,    45,    60, ...])
 
-        kernel = np.matmul(features, features.T)
-        change_points, _ = cpd_auto(kernel, seq_len - 1, 1)
+        kernel = np.matmul(features, features.T) # Matrix product of two arrays
+        change_points, _ = cpd_auto(kernel, seq_len - 1, 1) # Call of the KTS Function
         change_points *= 15
         change_points = np.hstack((0, change_points, n_frames))
         begin_frames = change_points[:-1]
         end_frames = change_points[1:]
         change_points = np.vstack((begin_frames, end_frames - 1)).T
+        # Here, the change points are detected (Change-point positions t0, t1, ..., t_{m-1})
 
+        # For each segment, calculate the number of frames
         n_frame_per_seg = end_frames - begin_frames
-
-        h5out.create_dataset(video_name + '/features', data=features)
-        h5out.create_dataset(video_name + '/gtscore', data=gtscore)
-        # h5out.create_dataset(name + '/user_summary', data=data_of_name)
-        h5out.create_dataset(video_name + '/change_points',
-                             data=change_points)
-        h5out.create_dataset(video_name + '/n_frame_per_seg',
-                             data=n_frame_per_seg)
-        h5out.create_dataset(video_name + '/n_frames', data=n_frames)
-        h5out.create_dataset(video_name + '/picks', data=picks)
+        
+        # Add the video info on the new dataset (that contain shots)
+        h5out.create_dataset(video_name + '/features', data=features)                   # add features
+        h5out.create_dataset(video_name + '/gtscore', data=gtscore)                     # add ground truth score  
+        # h5out.create_dataset(name + '/user_summary', data=data_of_name)   
+        h5out.create_dataset(video_name + '/change_points', data=change_points)         # add the change points
+        h5out.create_dataset(video_name + '/n_frame_per_seg', data=n_frame_per_seg)     # add number of frame for each segment
+        h5out.create_dataset(video_name + '/n_frames', data=n_frames)                   # add number of video frames
+        h5out.create_dataset(video_name + '/picks', data=picks)                         # add initial picks
         # h5out.create_dataset(video_name + '/n_steps', data=data_of_name)
-        h5out.create_dataset(video_name + '/gtsummary', data=gtsummary)
+        h5out.create_dataset(video_name + '/gtsummary', data=gtsummary)                 # add ground truth summary  
         # h5out.create_dataset(name + '/video_name', data=data_of_name)
 
     h5in.close()
